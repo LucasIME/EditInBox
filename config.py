ef7ca403d1d9c6e80d22f59a386ddd8303e9de75
Lucas Sousa Meireles <meireles31@msn.com>
2015-12-04 09:36:57 +0000

    added requirements.txt and config

---
+++ config.py
@@ -0,0 +1,14 @@
+__author__ = 'meirellu'
+
+UPLOAD_FOLDER = './uploadedFiles/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+APP_KEY = "qwmdl635urnxv8a"
+APP_SECRET = "j0z5rlprmbm2r3l"
+ACCESS_TYPE = 'app_folder'
+
+IMGUR_KEY = '88b6f6468266c01'
+IMGUR_SECRET = 'b8d2eb4d23efd057df375fab6a64ac7e10d86b74'
+
+dropboxKey = "PfypITz2Rm0AAAAAAABEM6WIGXQXw5CNZmqhPyXCxAwnkrD5ZOQ5R1MyVgOgKP88"
